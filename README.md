8f6618daa7233be9a1d745ebc6896ae5c876cb8e
Abhinav Saxena <abhinavsaxena927@gmail.com>
2023-12-16 20:14:15 +0530

    okay

---
+++ README.md
@@ -0,0 +1,20 @@
+This is a Habit Tracker  Project which is built using Nodejs, Ejs , CSS, Javascript
+
+
+## Features
+- Add Habit <br/>
+- Each Habit Track Weekly<br/>
+  - Mark Habit as Done
+  - Mark as Undone
+  - Toggle to Default - No action setting
+  - Delete a habit
+
+## Packages
+- body-parser <br/>
+- dotenv<br/>
+- ejs<br/>
+- express<br/>
+- mongoose<br/>
+- nodemon<br/>
+
+
---
+++ app/db.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+exports.connectMongoose =()=>{
+    mongoose.set("strictQuery", false);
+    mongoose.connect('mongodb+srv://codinav:kAPA30bYAh4goKQv@cluster0.jfmb5sl.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
+    .then((e)=>console.log("Connected to Mongodb => Habit-Tracker"))
+    .catch((e)=>console.log("Not Connect Mongodb", e));
+}
+
